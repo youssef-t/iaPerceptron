7e22371804130b5bd2f956a9f9a8810e447bdccb
omegaBionic <44264108+omegaBionic@users.noreply.github.com>
2020-11-12 11:03:30 +0100

    [Perceptron] Added training and test with predicts.

--- main.py
+++ main.py
@@ -32,28 +32,42 @@ total_moral_armies = 0
 for army in armies:
     total_moral_armies = total_moral_armies + army.get_total_moral()
 
-print(f"La valeur totale de moral de toutes les armées {total_moral_armies}")
+print(f"[Main] La valeur totale de moral de toutes les armées {total_moral_armies}")
 
-print("tableau_moral:\n{}\n".format(tableau_moral))
-print("tableau_boost:\n{}\n".format(tableau_boost))
-print("tableau_moral_total:\n{}\n".format(tableau_moral_total))
+print("[Main] tableau_moral:\n{}\n".format(tableau_moral))
+print("[Main] tableau_boost:\n{}\n".format(tableau_boost))
+print("[Main] tableau_moral_total:\n{}\n".format(tableau_moral_total))
 
 inputs = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
 expected_outputs = np.array([0, 0, 0, 1])
 
 # Init perceptron
-perceptron = perceptron.Perceptron(input_numbers=len(inputs), epoch=1, learning_rate=1)
+perceptron = perceptron.Perceptron(input_numbers=inputs, epochs=10000, learning_rate=10**(-3))
+
+# Calculate error value
+error_values = perceptron.surface_error(inputs, expected_outputs)
 
 # Train perceptron and get error values
-error_values = perceptron.train(inputs, expected_outputs)
+perceptron.train(inputs,expected_outputs)
+print("[Main] w0: {} - w1: {} - w2: {}".format(perceptron.get_w0(), perceptron.get_w1(), perceptron.get_w2()))
+
+# Predict with training for test training
+print("[Main] --- Test training ---")
+for input_, expected_output in zip(inputs, expected_outputs):
+    predicted = perceptron.predict_with_biais(input_, perceptron.get_w0(), perceptron.get_w1(), perceptron.get_w1())
+    if predicted == expected_output:
+        print("[Main] OK: for input_: {} expected_output: {} - predicted: {}".format(input_, expected_output, predicted))
+    else:
+        print("[Main] KO: for input_: {} expected_output: {} - predicted: {}".format(input_, expected_output, predicted))
+
 
 # Write weights to CSV
-print("w1: {} - w2: {}".format(perceptron.get_w1(), perceptron.get_w2()))
 with open('output.csv', 'w', newline='') as csvfile:
     spam_writer = csv.writer(csvfile, delimiter=';', quotechar='|', quoting=csv.QUOTE_MINIMAL)
-    spam_writer.writerow(["w1", "w2"])
-    spam_writer.writerow([perceptron.get_w1(), perceptron.get_w2()])
+    spam_writer.writerow(["w0", "w1", "w2"])
+    spam_writer.writerow([perceptron.get_w0(), perceptron.get_w1(), perceptron.get_w2()])
 
-# Display errors values
+# Display errors_values
+print("[Main] --- Display errors_values ---")
 plt.imshow(error_values)
 plt.show()
--- perceptron.py
+++ perceptron.py
@@ -2,16 +2,19 @@ import numpy as np
 
 
 class Perceptron:
-    def __init__(self, input_numbers, epoch, learning_rate):
+    def __init__(self, input_numbers, epochs, learning_rate=10**(-3)):
         self.input_numbers = input_numbers
-        self.epoch = epoch
+        self.epochs = epochs
         self.learning_rate = learning_rate
 
         self.w0 = 0.0
         self.w1 = 0.0
         self.w2 = 0.0
 
-        self.BIAIS = 1
+        self.BIAIS = 1.0
+
+    def get_w0(self):
+        return self.w0
 
     def get_w1(self):
         return self.w1
@@ -19,16 +22,29 @@ class Perceptron:
     def get_w2(self):
         return self.w2
 
-    def train(self, inputs, expected_outputs):
+    def surface_error(self, inputs, expected_outputs):
         error_values = np.zeros((11, 11))
         for i, raw in enumerate(inputs):
             # w1 and w2 between -5 and 5
-            for self.w1 in range(-5, 6):
-                for self.w2 in range(-5, 6):
-                    output = self.predict(raw)
+            for w1 in range(-5, 6):
+                for w2 in range(-5, 6):
+                    output = self.predict(raw, w1, w2)
                     error_value = self.calculate_error(output, expected_outputs[i])
-                    error_values[self.w1 + 5][self.w2 + 5] = error_values[self.w1 + 5][self.w2 + 5] + error_value
+                    error_values[w1 + 5][w2 + 5] = error_values[w1 + 5][w2 + 5] + error_value
         return error_values
+
+    def train(self, inputs, expected_outputs):
+        for epoch in range(self.epochs):
+            for i, raw in enumerate(inputs):
+                output = self.predict_with_biais(raw, self.w0, self.w1, self.w2)
+                print(f"[Perceptron] epoch: {epoch}")
+                print(f"[Perceptron] output for {raw[0]}, {raw[1]} : {output}")
+                print(f"[Perceptron] w0: {self.w0}, w1: {self.w1}, w2 {self.w1}")
+                print("[Perceptron] -------")
+
+                self.w0 = self.weight_widrow_hoff(self.w0, output, expected_outputs[i], self.BIAIS)
+                self.w1 = self.weight_widrow_hoff(self.w1, output, expected_outputs[i], raw[0])
+                self.w2 = self.weight_widrow_hoff(self.w2, output, expected_outputs[i], raw[1])
 
     def activation_function(self, x):
         if x <= 0:
@@ -36,12 +52,21 @@ class Perceptron:
         else:
             return 1
 
-    def predict(self, inputs_local):
-        ret_0 = self.BIAIS * self.w0
-        ret_1 = inputs_local[0] * self.w1
-        ret_2 = inputs_local[1] * self.w2
+    def predict(self, inputs_local, w1, w2):
+        ret_1 = inputs_local[0] * w1
+        ret_2 = inputs_local[1] * w2
+
+        return self.activation_function(ret_1 + ret_2)
+
+    def predict_with_biais(self, inputs_local, w0, w1, w2):
+        ret_0 = self.BIAIS * w0
+        ret_1 = inputs_local[0] * w1
+        ret_2 = inputs_local[1] * w2
 
         return self.activation_function(ret_0 + ret_1 + ret_2)
 
     def calculate_error(self, output_local, expected_output):
         return 0.5 * (output_local - expected_output) ** 2
+
+    def weight_widrow_hoff(self, weight, output, expected_output, input):
+        return weight + self.learning_rate * (expected_output - output) * input
